7e2f6633243aa1717250554b225e08673c6cb1ae
Megan McKissack <meganmckissack@gmail.com>
2022-05-24 21:02:19 -0700

    update to get multiple gifs from search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import GiphyService from './giphy-service';
 
 
@@ -10,13 +11,17 @@ function clearFields() {
 }
 
 $(document).ready(function() {
-  $('#gifSearch').click(function() {
+  $('#formOne').submit(function(event) {
+    event.preventDefault();
     let gif = $('#gif').val();
     clearFields();
     let promise = GiphyService.getGif(gif);
     promise.then(function(response) {
       const responseData = JSON.parse(response);
-      $('.showGifs').prepend(`<img src=${responseData.data[0].url}>`);
+      for  (let i = 0; i < 10; i++) {
+        $('.showGifs').append("<img src=" + `${responseData.data[i].images.downsized.url}` + " />");
+      } 
+      
       // $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
     }, function(error) {
